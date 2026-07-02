fd411bca9f8f36f3a324d9cef2cd1b76f8dd03e4
Sinchu <sinchukm312@gmail.com>
2026-07-02 15:09:32 +0530

    Initial commit - Terraform ACR setup

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+resource_group_name = "acr-resource-group"
+location            = "Central India"
+acr_name            = "snehaacr2026demo123"
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+}
+
+variable "location" {
+  description = "Azure region"
+}
+
+variable "acr_name" {
+  description = "Azure Container Registry name"
+}
